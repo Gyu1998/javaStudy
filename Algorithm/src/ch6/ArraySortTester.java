fcdf60c6b89c1ea8dc1e1491aedf5413cd6e6849
규 <dhfpswl0709@gmail.com>
2024-07-02 18:32:06 +0900

    2024.07.02-18:31

---
+++ Algorithm/src/ch6/ArraySortTester.java
@@ -0,0 +1,27 @@
+package ch6;
+import java.util.Arrays;
+import java.util.Scanner;
+//Arrays.sort 메서드를 사용하여 정렬(퀵 정렬)
+
+public class ArraySortTester {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요솟수:");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		for (int i=0; i<num; i++) {
+			System.out.print("x[" + i + "]:");
+			x[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(x);
+		
+		System.out.println("오름차순으로 정렬했습니다");
+		for(int i=0; i<num; i++) 
+			System.out.println("x[" + i + "]=" + x[i]);
+	}
+
+}
